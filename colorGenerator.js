38baa8e3c9a341992da31375b9bceb17cd776a2d
Muhammad Awais Danish <mr.awaisdanish@gmail.com>
2022-08-04 08:56:00 +0500

    ..

--- colorGenerator.js
+++ colorGenerator.js
@@ -1,6 +1,6 @@
-const redRange = document.querySelector('#red');
-const greenRange = document.querySelector('#green');
-const blueRange = document.querySelector('#blue'); 
+const redSlider = document.querySelector('#red');
+const greenSlider = document.querySelector('#green');
+const blueSlider = document.querySelector('#blue'); 
 
 const valueOfRed = document.querySelector('#ValueOfRedColor');
 const valueOfGreen = document.querySelector('#ValueOfGreenColor');
@@ -9,30 +9,11 @@ const valueOfBlue = document.querySelector('#ValueOfBlueColor');
 const colorBox = document.querySelector('#colorBox');
 const colorValue = colorBox.querySelector('#colorBox > span');
 
-let red = 0;
-let green = 0;
-let blue = 0;
 
-redRange.addEventListener('input' , e => {
-    console.log();
-    red = e.target.value;
-    renderHTML();
-});
-
-greenRange.addEventListener('input' , e => {
-    green = e.target.value;
-    renderHTML();
-});
-
-blueRange.addEventListener('input' , e => {
-    blue = e.target.value;
-    renderHTML();
-});
-
-const renderHTML = () => {
-    colorBox.style.backgroundColor = `rgb(${red} , ${green} , ${blue})`;
-    colorValue.textContent = `rgb ( ${red} , ${green} , ${blue} )`;
-    valueOfRed.textContent = red;
-    valueOfGreen.textContent = green;
-    valueOfBlue.textContent = blue;
+const generateColor = () => {
+    colorBox.style.backgroundColor = `rgb(${redSlider.value} , ${greenSlider.value} , ${blueSlider.value})`;
+    colorValue.textContent = `rgb ( ${redSlider.value} , ${greenSlider.value} , ${blueSlider.value} )`;
+    valueOfRed.textContent = redSlider.value;
+    valueOfGreen.textContent = greenSlider.value;
+    valueOfBlue.textContent = blueSlider.value;
 }
